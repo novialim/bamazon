796f7116341fa4ce10a639a7af9f494e84f00d1f
novialim <novialim@gmail.com>
2017-07-06 13:51:02 -0700

    Basic Customer View Completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,6 +7,7 @@
 // However, if your store does have enough of the product, you should fulfill the customer's order.
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
+
 var figlet = require('figlet');
 var mysql = require('mysql');
 var Table = require('cli-table2');
